28ac07c8a1e0f5637ba19b00477423224ab91138
Leonardo Gonçalves Serafin <leogserafin@hotmail.com>
2018-08-21 11:37:07 -0300

    Primeiro trabalho

---
+++ exercicio-01/exercicio4.java
@@ -0,0 +1,4 @@
+public static void main(double[] args) {
+            double IMC = args[0]/ (args[1]*args[1]);
+            System.out.println("Seu IMC é: "+IMC);
+    }
---
+++ exercicio-01/exercicio5.java
@@ -0,0 +1,8 @@
+public static void main(double[] args) {
+        
+        if(args[0] < 21){
+            System.out.println("voce ganhou: "+args[0]*args[1]);
+        }else{
+            System.out.println("erro");
+        }
+    }
