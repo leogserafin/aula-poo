6a88e936c32577e44f72155fa00f2bb12fcecd9f
Leonardo Goncalves Serafin <6093914@univali.br>
2018-10-02 13:17:02 -0300

    trabalhos em mobile feitos

---
+++ Calculadora de IMC/app/src/main/java/serafin/univali/br/contatoandroid/MainActivity.java
@@ -0,0 +1,48 @@
+package serafin.univali.br.contatoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+        public void CalculoIMC(View v){
+            EditText peso = findViewById(R.id.etPeso);
+            EditText altura = findViewById(R.id.etAltura);
+            TextView imc = findViewById(R.id.tIMC);
+
+            String alturaDigitada = altura.getText().toString();
+            String pesoDigiado = peso.getText().toString();
+
+            if(alturaDigitada.equals("") || pesoDigiado.equals("")){
+                altura.setError("Informe a altura!");
+                peso.setError("Informe o peso!");
+                return;
+            }
+
+            try {
+                double alturaEmDouble = Double.parseDouble(alturaDigitada);
+                double pesoEmDouble = Double.parseDouble(pesoDigiado);
+
+                double resultadoIMC = pesoEmDouble/(alturaEmDouble*alturaEmDouble);
+                imc.setText("IMC: "+resultadoIMC);
+
+                imc.setVisibility(View.VISIBLE);
+            }catch (Exception e){
+                imc.setText("errou!!!");
+            }
+
+
+        }
+}
